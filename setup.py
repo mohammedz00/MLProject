521773ee3ca577afe6904fb6e7637dfb0ced111a
mohammedz00 <zenudeens@gmail.com>
2026-01-20 09:58:51 +0000

    Day 1 Complete

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     This function returns a list of the requirements
@@ -8,12 +9,17 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace('\n','') for req in requirments]
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 setup(
 name='mlproject',
-version=0.0.1,
+version='0.0.1',
 author='Zenudeen',
-packages=find_packages,
+packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 )
