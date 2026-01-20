8882795f9b8792bf12d5548a929a02cd26b5fb47
mohammedz00 <zenudeens@gmail.com>
2026-01-20 09:42:23 +0000

    Created get_requirements function

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns a list of the requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirments]
 
 setup(
 name='mlproject',
 version=0.0.1,
 author='Zenudeen',
 packages=find_packages,
-install_requires=['pandas','numpy','seaborn']
+install_requires=get_requirements('requirements.txt')
 )
